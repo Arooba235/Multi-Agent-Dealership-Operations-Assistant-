df885cc67a3fea5ccbe1d8522d27a0cf664521bb
Arooba235 <aroobamaqsood235@gmail.com>
2025-11-17 08:16:13 +0500

    update

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from helper import graph
+import json
 app = Flask(__name__)
 
 @app.route('/')
@@ -19,6 +20,19 @@ def analyze():
     
     # Return the response as JSON
     return jsonify({'response': response})
+
+@app.route("/get-customer")
+def get_customer():
+    with open("customer.json", "r") as f:
+        data = json.load(f)
+    return jsonify(data)
+
+@app.route("/save-customer", methods=["POST"])
+def save_customer():
+    data = request.get_json()
+    with open("customer.json", "w") as f:
+        json.dump(data, f, indent=2)
+    return jsonify({"status": "saved"})
 
 if __name__ == '__main__':
     app.run(debug=True)
